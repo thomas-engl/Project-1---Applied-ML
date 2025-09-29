9895a84eb27e32a2d624ec8a0d9f1d4190fbefcf
PhilippBruckelt <philippb1610@gmail.com>
2025-09-29 23:20:36 +0200

    some changes

--- Code/Project1a_b.py
+++ Code/Project1a_b.py
@@ -14,13 +14,16 @@ from matplotlib import cm
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
+### random seed
+np.random.seed(123)
+
 # Runge function
 def runge(x): return 1 / (1 + 25*x**2)
 
 
 # create feature matrix 
 
-def feature_matrix(x, y, deg):
+def feature_matrix(x, deg):
     mat = np.zeros((len(x), deg + 1))
     mat[:, 0] = 1
     for i in range(1, deg + 1):
@@ -37,15 +40,25 @@ def scale_matrix(matrix):
     mean = np.array([np.mean(matrix[:,i]) for i in range(p)])
     std = np.array([np.sqrt(variance_array(matrix[:,i])) for i in range(p)])
     std[std==0] = 1
-    return (matrix - mean) / std
+    # for every column, subtract the mean and divide by the standard deviation
+    # finally, remove the first column which is just 0
+    return ((matrix - mean) / std)[:,1:]
 
 def get_scaled_data(x, y, deg):
-    X = feature_matrix(x_pts, y_pts, deg)
+    X = feature_matrix(x, deg)
     # scale data
     X_norm = scale_matrix(X)
-    y_centered = y_pts - np.mean(y_pts)
+    y_centered = y - np.mean(y)
     # return feature matrix and targets splitted in test and train data
     return train_test_split(X_norm, y_centered)
+
+"""
+def get_scaled_data(x, y, deg):
+    X = feature_matrix(x, y, deg)
+    # scale data
+    y_centered = y - np.mean(y)
+    X_norm = 
+"""
 
 # ordinary least squares
 
@@ -74,19 +87,18 @@ def r2(y_est, y_true):
     y_mean = 1/len(y_true) * np.sum(y_true)
     denominator = np.sum((y_true - y_mean)**2)
     nominator = np.sum((y_true - y_est)**2)
-    return 1 - nominator / denominator
-
+    return 1 - nominator / denominator 
+    
 # plot results
 def plot_results(x_values, results_ols, results_ridge, quantity='MSE'):
     """ plots results for OLS and Ridge, quantity is either 'MSE' or 'R2' """
     plt.figure(dpi=150)
     plt.plot(x_values, results_ols, '-D', color='red', label='OLS')
     plt.plot(x_values, results_ridge, '-D', color='blue', label='Ridge')
+    plt.legend()
     plt.grid()
     plt.xlabel("polynomial degree")
     plt.ylabel('MSE' if quantity=='MSE' else r'$R^2$ score')
-    plt.legend()
-    plt.show()
     
 ### create heat map for ridge regression    
 
@@ -109,11 +121,17 @@ def heat_map_ridge(params, degs, values, quantity='MSE'):
     fig.tight_layout()
     plt.show()
     
-def polynomial(coefficients, x):
-    return sum(coefficients[i] * x**i for i in range(len(coefficients)))
+def polynomial(coefficients, x, intercept=True):
+    if intercept:
+        return sum(coefficients[i] * x**i for i in range(len(coefficients)))
+    else:
+        return sum(coefficients[i] * x**(i+1) for i in range(len(coefficients)))
 
 # vectorize the function
-polynomial_v = np.vectorize(polynomial, excluded={0, 'coefficients'})
+polynomial_v = np.vectorize(polynomial, excluded={0, 2})
+
+
+# In[1]:
 
 """
 Numerical Experiment 1
@@ -149,7 +167,7 @@ for deg in degs:
     y_predict_ridge = X_test @ beta_ridge
     mses_ridge.append(mse(y_predict_ridge, y_test))
     r2_ridge.append(r2(y_predict_ridge, y_test))
-    
+
 """ Plot the data """
 
 # figure for MSEs
@@ -160,10 +178,26 @@ plot_results(degs, np.array(r2_ols), np.array(r2_ridge), quantity='R2')
 """ Plot coefficients """
 
 plt.figure(dpi=150)
-for beta in betas_ols:
-    plt.scatter(np.arange(len(beta)), np.array(beta))
+colors = ['blue', 'red', 'green']
+for (i, beta) in enumerate(betas_ols[4::5]):
+    markerline, stemlines, baseline = plt.stem(
+        np.arange(1, len(beta) + 1) + i/4, np.array(beta), linefmt=colors[i], 
+        bottom=0, label=r'$\deg = {}$'.format(5*(i+1)))
+    markerline.set_markerfacecolor(colors[i])
+    # hide baseline in the for loop and create one common base line later
+    baseline.set_visible(False)
+
+# maximal coefficient - used for y-limit
+m = np.max(np.array([np.max(np.abs(i)) for i in betas_ols[4::5]]))
+
+plt.axhline(0, color="grey", linestyle="--", linewidth=1)
+plt.grid()
 plt.xlabel(r'$i$')
+plt.xticks(np.arange(1, 16))
 plt.ylabel(r'$\beta_i$')
+plt.yscale('symlog', linthresh=1)
+plt.ylim(-1.5*m, 1.5*m)
+plt.legend()
 plt.show()
 
 """ Plot the approximating polynomial for deg 5, 10, 15 """
@@ -171,12 +205,15 @@ plt.show()
 plt.figure(dpi=150)
 x = np.linspace(-1, 1, 101)
 for i, beta in enumerate(betas_ols[4::5]):
-    plt.plot(x, polynomial_v(beta, x), label=r'$\deg = {}$'.format(5*(i+1)))
-# plt.plot(x, runge(x), label=r'$f(x)$')
+    plt.plot(x, polynomial_v(beta, x, intercept=False), 
+             label=r'$\deg = {}$'.format(5*(i+1)))
+# plt.plot(x, runge(x) - np.mean(runge(x)), label=r'$f(x)$')
 plt.grid()
 plt.legend()
 plt.show()
 
+
+# In[2]:
 
 """
 Numerical Experiment 2
@@ -204,3 +241,4 @@ for param in lambdas:
 parameters """
     
 heat_map_ridge(lambdas, degs, mses)
+heat_map_ridge(lambdas, degs, r2_scores, quantity='R2')
